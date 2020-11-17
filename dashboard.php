0580a6af5c0c19b0c782fec90a6d5961a0191108
Luiz <luiz-dh@hotmail.com>
2020-11-17 20:37:55 -0300

    Org1

---
+++ dashboard.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<!-- saved from url=(0014)about:internet -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="https://getbootstrap.com.br/favicon.ico">
+
+    <title>Luiz Aula</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  </head>
+
+  <body>
+    <nav class="navbar navbar-dark bg-dark flex-md-nowrap p-0 shadow">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="https://getbootstrap.com.br/docs/4.1/examples/dashboard/#">Luiz Antonio</a>
+      
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="https://getbootstrap.com.br/docs/4.1/examples/dashboard/#">Sair</a>
+        </li>
+      </ul>
+    </nav>
+
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="https://getbootstrap.com.br/docs/4.1/examples/dashboard/#">
+                Dashboard <span class="sr-only">(atual)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://getbootstrap.com.br/docs/4.1/examples/dashboard/#">  
+                  Pedidos
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://getbootstrap.com.br/docs/4.1/examples/dashboard/#">
+                  Produtos
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://getbootstrap.com.br/docs/4.1/examples/dashboard/#">     
+                  Clientes
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://getbootstrap.com.br/docs/4.1/examples/dashboard/#">
+                Relatórios
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://getbootstrap.com.br/docs/4.1/examples/dashboard/#">     
+                  Integrações
+                </a>
+              </li>
+            </ul>
+
+            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+              <span>Relatórios salvos</span>
+              <a class="d-flex align-items-center text-muted" href="https://getbootstrap.com.br/docs/4.1/examples/dashboard/#">                
+              </a>
+            </h6>
+            <ul class="nav flex-column mb-2">
+              <li class="nav-item">
+                <a class="nav-link" href="https://getbootstrap.com.br/docs/4.1/examples/dashboard/#">
+                Neste mês
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://getbootstrap.com.br/docs/4.1/examples/dashboard/#">
+                Último trimestre
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://getbootstrap.com.br/docs/4.1/examples/dashboard/#">
+                  
+                  Engajamento social
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://getbootstrap.com.br/docs/4.1/examples/dashboard/#">
+                  
+                  Vendas do final de ano
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+
+        <!-- conteudo -->
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Dashboard</h1>
+            <div class="btn-toolbar mb-2 mb-md-0">
+              <div class="btn-group mr-2">
+                
+              </div>
+              
+            </div>
+          </div>
+
+      
+
+          <h2>Título da seção</h2>
+          
+        </main>
+      </div>
+    </div>
+
+    <!-- Principal JavaScript do Bootstrap
+    ================================================== -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+    
+  
+
+</body></html>
